1fb046f756610c1d3ba118b6189cfeee43e69acf
raihanM95 <mahmudrony95@gmail.com>
2019-06-16 10:12:27 +0600

    Lab Test-1

---
+++ LabTest/LabTest/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace LabTest
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.soldierNoTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.soldierNameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.score1TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.score2TextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.score3TextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.score4TextBox = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.searchByComboBox = new System.Windows.Forms.ComboBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.averageScorerTextBox = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.totalScorerTextBox = new System.Windows.Forms.TextBox();
+            this.checkSoldierNoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Soldier Score Register";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soldier No";
+            // 
+            // soldierNoTextBox
+            // 
+            this.soldierNoTextBox.Location = new System.Drawing.Point(133, 59);
+            this.soldierNoTextBox.Name = "soldierNoTextBox";
+            this.soldierNoTextBox.Size = new System.Drawing.Size(144, 20);
+            this.soldierNoTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Soldier Name";
+            // 
+            // soldierNameTextBox
+            // 
+            this.soldierNameTextBox.Location = new System.Drawing.Point(133, 85);
+            this.soldierNameTextBox.Name = "soldierNameTextBox";
+            this.soldierNameTextBox.Size = new System.Drawing.Size(144, 20);
+            this.soldierNameTextBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Target 1 Score";
+            // 
+            // score1TextBox
+            // 
+            this.score1TextBox.Location = new System.Drawing.Point(133, 111);
+            this.score1TextBox.Name = "score1TextBox";
+            this.score1TextBox.Size = new System.Drawing.Size(106, 20);
+            this.score1TextBox.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Target 2 Score";
+            // 
+            // score2TextBox
+            // 
+            this.score2TextBox.Location = new System.Drawing.Point(133, 137);
+            this.score2TextBox.Name = "score2TextBox";
+            this.score2TextBox.Size = new System.Drawing.Size(106, 20);
+            this.score2TextBox.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Target 3 Score";
+            // 
+            // score3TextBox
+            // 
+            this.score3TextBox.Location = new System.Drawing.Point(133, 163);
+            this.score3TextBox.Name = "score3TextBox";
+            this.score3TextBox.Size = new System.Drawing.Size(106, 20);
+            this.score3TextBox.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Target 4 Score";
+            // 
+            // score4TextBox
+            // 
+            this.score4TextBox.Location = new System.Drawing.Point(133, 189);
+            this.score4TextBox.Name = "score4TextBox";
+            this.score4TextBox.Size = new System.Drawing.Size(106, 20);
+            this.score4TextBox.TabIndex = 2;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(202, 228);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(398, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Soldier Information";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(398, 59);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Search Criteria";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(398, 85);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Search By";
+            // 
+            // searchByComboBox
+            // 
+            this.searchByComboBox.FormattingEnabled = true;
+            this.searchByComboBox.Items.AddRange(new object[] {
+            "Soldier No",
+            "Soldier Name"});
+            this.searchByComboBox.Location = new System.Drawing.Point(470, 82);
+            this.searchByComboBox.Name = "searchByComboBox";
+            this.searchByComboBox.Size = new System.Drawing.Size(121, 21);
+            this.searchByComboBox.TabIndex = 4;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(470, 106);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(121, 20);
+            this.searchTextBox.TabIndex = 5;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(597, 103);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 6;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(597, 140);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 7;
+            this.ShowButton.Text = "Show All";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(401, 192);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(376, 176);
+            this.showRichTextBox.TabIndex = 8;
+            this.showRichTextBox.Text = "";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(401, 386);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(103, 13);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Top Average Scorer";
+            // 
+            // averageScorerTextBox
+            // 
+            this.averageScorerTextBox.Location = new System.Drawing.Point(510, 383);
+            this.averageScorerTextBox.Name = "averageScorerTextBox";
+            this.averageScorerTextBox.Size = new System.Drawing.Size(181, 20);
+            this.averageScorerTextBox.TabIndex = 10;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(417, 412);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(87, 13);
+            this.label12.TabIndex = 9;
+            this.label12.Text = "Top Total Scorer";
+            // 
+            // totalScorerTextBox
+            // 
+            this.totalScorerTextBox.Location = new System.Drawing.Point(510, 409);
+            this.totalScorerTextBox.Name = "totalScorerTextBox";
+            this.totalScorerTextBox.Size = new System.Drawing.Size(181, 20);
+            this.totalScorerTextBox.TabIndex = 10;
+            // 
+            // checkSoldierNoLabel
+            // 
+            this.checkSoldierNoLabel.AutoSize = true;
+            this.checkSoldierNoLabel.Location = new System.Drawing.Point(284, 62);
+            this.checkSoldierNoLabel.Name = "checkSoldierNoLabel";
+            this.checkSoldierNoLabel.Size = new System.Drawing.Size(0, 13);
+            this.checkSoldierNoLabel.TabIndex = 11;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkSoldierNoLabel);
+            this.Controls.Add(this.totalScorerTextBox);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.averageScorerTextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchByComboBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.score4TextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.score3TextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.score2TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.score1TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.soldierNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.soldierNoTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Name = "Form";
+            this.Text = "Fire Range Automation System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox soldierNoTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox soldierNameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox score1TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox score2TextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox score3TextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox score4TextBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox searchByComboBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox averageScorerTextBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox totalScorerTextBox;
+        private System.Windows.Forms.Label checkSoldierNoLabel;
+    }
+}
+
---
+++ LabTest/LabTest/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabTest
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        List<string> soldiersNo = new List<string>();
+        List<string> soldierNames = new List<string>();
+        List<int> targets1 = new List<int>();
+        List<int> targets2 = new List<int>();
+        List<int> targets3 = new List<int>();
+        List<int> targets4 = new List<int>();
+        List<int> averages = new List<int>();
+        List<int> totals = new List<int>();
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                checkSoldierNoLabel.Text = "";
+
+                
+
+                string soldierNo = soldierNoTextBox.Text;
+                string soldierName = soldierNameTextBox.Text;
+
+                if (SoldierExists(soldierNo))
+                {
+                    checkSoldierNoLabel.Text = "Soldier Already Exist!";
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(score1TextBox.Text))
+                {
+                    MessageBox.Show("This field is required");
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(score2TextBox.Text))
+                {
+                    MessageBox.Show("This field is required");
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(score3TextBox.Text))
+                {
+                    MessageBox.Show("This field is required");
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(score4TextBox.Text))
+                {
+                    MessageBox.Show("This field is required");
+                    return;
+                }
+
+                int target1 = Convert.ToInt32(score1TextBox.Text);
+                int target2 = Convert.ToInt32(score2TextBox.Text);
+                int target3 = Convert.ToInt32(score3TextBox.Text);
+                int target4 = Convert.ToInt32(score4TextBox.Text);
+
+                
+
+                soldiersNo.Add(soldierNo);
+                soldierNames.Add(soldierName);
+                targets1.Add(target1);
+                targets2.Add(target2);
+                targets3.Add(target3);
+                targets4.Add(target4);
+
+                for(int index = 0; index < soldiersNo.Count; index++)
+                {
+                    for (int index2 = 0; index2 < soldiersNo.Count; index2++)
+                    {
+                        totals.Add(targets1[index2] + targets2[index2] + targets3[index2] + targets4[index2]);
+                    }
+                }
+
+                for (int index = 0; index < soldiersNo.Count; index++)
+                {
+                    for (int index2 = 0; index2 < soldiersNo.Count; index2++)
+                    {
+                        averages.Add((targets1[index2] + targets2[index2] + targets3[index2] + targets4[index2])/4);
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            showRichTextBox.Text = Display();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if(searchByComboBox.Text == "Soldier No")
+            {
+                int index = 0;
+                foreach (string soldierNo in soldiersNo)
+                {
+                    if (soldierNo == searchTextBox.Text)
+                        MessageBox.Show(soldierNo + " " + soldierNames[index]);
+                    else
+                        MessageBox.Show("Not found");
+                    index++;
+                }
+            }
+            else if(searchByComboBox.Text == "Soldier Name")
+            {
+                int index = 0;
+                foreach (string soldierName in soldierNames)
+                {
+                    if (soldierName == searchTextBox.Text)
+                        MessageBox.Show(soldiersNo[index] + " " + soldierNames[index]);
+                    else
+                        MessageBox.Show("Not found");
+                    index++;
+                }
+            }
+        }
+
+        private string Display()
+        {
+            string message = "";
+            message = " Soldier No.\t| Soldier name\t| Average Score\t| Total Score\n";
+
+            for (int index = 0; index < soldiersNo.Count; index++)
+            {
+                message = message + " " + soldiersNo[index] + "\t " + soldierNames[index] + "\t " + averages[index] + "\t " + totals[index] + "\n";
+                //"\t " + averages[index] + "\t " + totals[index] +
+            }
+
+            return message;
+        }
+
+        private bool SoldierExists(string soldier)
+        {
+            bool isExists = false;
+
+            foreach (string soldierCheck in soldiersNo)
+            {
+                if (soldierCheck == soldier)
+                    isExists = true;
+            }
+            return isExists;
+        }
+    }
+}
